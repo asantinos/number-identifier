8df9473beb5c645ccc048bf8ba440d1beac9c368
Alex <aleexsantos1509@gmail.com>
2023-10-03 10:41:54 +0200

    Identify positive, negative or zero

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+let numero = prompt("Ingresa un número:");
+numero = parseFloat(numero);
+
+if (!isNaN(numero)) {
+  if (numero > 0) {
+    console.log("El número ingresado es positivo.");
+    alert("El número ingresado es positivo.");
+  } else if (numero < 0) {
+    console.log("El número ingresado es negativo.");
+    alert("El número ingresado es negativo.");
+  } else {
+    console.log("El número ingresado es igual a cero.");
+    alert("El número ingresado es igual a cero.");
+  }
+} else {
+  alert("No ingresaste un número válido.");
+}
